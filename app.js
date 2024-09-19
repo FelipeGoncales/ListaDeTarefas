2a8ee97c01149bb771e7c401af8a2a42322bede2
FelipeGoncales <felipeggoncales22@gmail.com>
2024-09-19 16:58:46 -0300

    alterações

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     const nomeTarefa = document.getElementById('text-input').value;
     const btnSubmit = document.getElementById('btn-submit');
+
+    btnSubmit.addEventListener('click', (event) => {
+
+        const nome = document.getElementById('text-input').value.trim();
+        const data = document.getElementById('date-input').value.trim()
+        
+        if (!nome || !data) {
+            alert('É necessário informar a data e título da tarefa para continuar');
+        } else {
+            let ul = document.getElementById('ul1');
+            let li = document.createElement('li');
+
+            let textInput = document.createElement('input');
+            textInput.type = 'text';
+            textInput.value = nome;
+            textInput.disabled = true;
+
+            let checkInput = document.createElement('input');
+            checkInput.type = 'checkbox';
+
+            let dateInput = document.createElement('input');
+            dateInput.type = 'date';
+            dateInput.value = data;
+
+            li.classList.add('container');
+            li.append(checkInput, textInput, dateInput);
+
+            ul.appendChild(li);
+        }
+    }
+    )
 }
 )
 
