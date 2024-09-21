c93828e6488d121ffdc20e7fd4e47164fda18d65
Felipe Gonçales <felipeggoncales22@gmail.com>
2024-09-20 23:57:44 -0300

    adicionando funcionalidades (js)
    - Editar
    - Excluir
    - Checar

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const nomeTarefa = document.getElementById('text-input').value;
     const btnSubmit = document.getElementById('btn-submit');
 
-    btnSubmit.addEventListener('click', (event) => {
-
+    btnSubmit.addEventListener('click', () => {
         const nome = document.getElementById('text-input').value.trim();
-        const data = document.getElementById('date-input').value.trim()
+        const data = document.getElementById('date-input').value.trim();
         
         if (!nome || !data) {
             alert('É necessário informar a data e título da tarefa para continuar');
         } else {
-            let ul = document.getElementById('ul1');
-            let li = document.createElement('li');
+            const ul = document.getElementById('ul1');
+            const li = document.createElement('li');
 
             let textInput = document.createElement('input');
             textInput.type = 'text';
@@ -20,24 +18,90 @@ document.addEventListener('DOMContentLoaded', () => {
 
             let checkInput = document.createElement('input');
             checkInput.type = 'checkbox';
+            checkInput.addEventListener('click', function() {
+                let ulTarefas = document.getElementById('ul1');
+                let ulConcluidas = document.getElementById('ul2');
+                let li = this.parentNode;
+                if (this.checked) {
+                    ulConcluidas.appendChild(li);
+                } else {
+                    ulTarefas.appendChild(li)
+                }
+                // Adicionar mensagem a ul concluídas caso ela esteja vazia 
+                if (ulConcluidas.firstElementChild.nodeName === 'P') {
+                    ulConcluidas.firstElementChild.remove()
+                }
+                // Adicionar mensagem a ul tarefas caso ela esteja vazia 
+                if (ulTarefas.firstElementChild.nodeName === 'P') {
+                    ulTarefas.firstElementChild.remove()
+                }
+
+                if (ulTarefas.length === 0) {
+                    addMensagem('ul1');
+                }
+                if (ulConcluidas.length === 0) {
+                    addMensagem('ul2')
+                } 
+            })
 
             let dateInput = document.createElement('input');
             dateInput.type = 'date';
             dateInput.value = data;
+            dateInput.disabled = true;
+
+            let lixeiraIcon = document.createElement('i');
+            lixeiraIcon.classList.add('fa-regular', "fa-trash-can");
+            lixeiraIcon.addEventListener("click", function() {
+                this.parentNode.remove();
+                let ul = document.getElementById('ul1');
+                if (ul.children.length === 0) {
+                    addMensagem('ul1');
+                }
+            })
+
+            let editIcon = document.createElement('i');
+            editIcon.classList.add('fa-solid', 'fa-pen-to-square');
+            editIcon.addEventListener('click', function() {
+                let icon = this;
+                let li = icon.parentNode;
+                let inputText = li.querySelector('input[type="text"]');
+                let inputDate = li.querySelector('input[type="date"]');
+
+                if (inputText.disabled === true || inputDate.disabled === true) {
+                    inputText.disabled = false;
+                    inputDate.disabled = false;
+                    icon.classList.replace('fa-pen-to-square', 'fa-check');
+                } else {
+                    inputText.disabled = true;
+                    inputDate.disabled = true;
+                    icon.classList.replace('fa-check', 'fa-pen-to-square');
+                }
+            })
 
             li.classList.add('container');
-            li.append(checkInput, textInput, dateInput);
+            li.append(checkInput, textInput, dateInput, editIcon, lixeiraIcon);
 
             ul.appendChild(li);
+
+            if (ul.firstElementChild.nodeName === 'P') {
+                ul.firstElementChild.remove();
+            }
         }
-    }
-    )
+    });
+});
+
+function addMensagem(ul) {
+    let ulTag = document.getElementById(ul);
+
+    let p = document.createElement('p');
+    p.innerText = 'Nenhuma tarefa encontrada';
+    p.classList.add('mensagem-vazio');
+    ulTag.appendChild(p);
 }
-)
 
 function bandeja(n) {
-    const ul = document.getElementById('ul'+n);
-    const seta = document.getElementById('seta'+n);
+    const ul = document.getElementById('ul' + n);
+    const seta = document.getElementById('seta' + n);
 
     const ulDisplay = window.getComputedStyle(ul).display;
 
